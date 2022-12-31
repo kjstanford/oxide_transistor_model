d7358be622100cc1e6e9d3d6e2a0b99dbd96a108
Koustav Jana <120333000+kjstanford@users.noreply.github.com>
2022-12-31 01:31:06 -0800

    Add files via upload

--- Python_based_models/va_py_model_evaluation.py
+++ Python_based_models/va_py_model_evaluation.py
@@ -5,42 +5,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from ITO_analytical_model_ver1 import ITO_analytical_model as model
 from numpy import pi, log, log10, exp
-from cascade_csv_reader_full import cascade_csv_reader, lin_plot, logx_plot, logy_plot
+from cascade_csv_reader import cascade_csv_reader, lin_plot, logx_plot, logy_plot
 import fileinput as fi
   
-lines = list(fi.input(files = 'evaluate_12_30_v2.lis'))
+lines = list(fi.input(files ='evaluate_12_30_v2.lis'))
 
 line_num = 0
 first_word = lines[line_num].split()
 first_word = None if len(first_word) == 0 else first_word[0]
 acq_start = 0
 acq_list = []
-while True:
-     if first_word == 'y':
-          acq_start = 0
+while first_word != 'y':
      if acq_start == 1:
-          acq_list[-1].append(lines[line_num].split())
+          acq_list.append(lines[line_num].split())
      if first_word == 'volt':
           line_num += 2
           acq_start = 1
-          acq_list.append([])
      else:
           line_num += 1
-     if line_num == len(lines):
-          break
      first_word = lines[line_num].split()
      first_word = None if len(first_word) == 0 else first_word[0]
 
 suffix_dict = {'a':1e-18, 'f':1e-15, 'p':1e-12, 'n':1e-9, 'u':1e-6, 'm':1e-3}
-num_devices = len(acq_list[0][0]) - 1
+num_devices = len(acq_list[0]) - 1
 print(num_devices)
-Vg_acq_lists = [[float(x[0][:-1])*suffix_dict[x[0][-1]] if x[0][-1] in suffix_dict else float(x[0]) for x in xx] \
-               for y in range(1, num_devices+1) for xx in acq_list]
-Id_acq_lists = [[float(x[y][:-1])*suffix_dict[x[y][-1]] if x[y][-1] in suffix_dict else float(x[y]) for x in xx] \
-               for y in range(1, num_devices+1) for xx in acq_list]
-Vd_list = [0.2, 0.4, 0.6, 0.8, 1]
-Vd_acq_lists = [[Vd for x in xx] for y in range(1, num_devices+1) for xx, Vd in zip(acq_list, Vd_list)]
-print(len(Vd_acq_lists), len(Vg_acq_lists), len(Id_acq_lists))
+Vg_acq_list = [float(x[0][:-1])*suffix_dict[x[0][-1]] if x[0][-1] in suffix_dict else float(x[0]) for x in acq_list]
+Id_acq_lists = [[float(x[y][:-1])*suffix_dict[x[y][-1]] if x[y][-1] in suffix_dict else float(x[y]) for x in acq_list] \
+     for y in range(1, num_devices+1)]
+print(Vg_acq_list, Id_acq_lists)
      
           
 
@@ -75,8 +67,7 @@ params = dict(L=L, W=W, mu_band=mu_band, Ntraps=Ntraps*frac, Ttraps=Ttraps, Nch=
 Cox = epso*params['kDE']/params['tDE']
 
 ax_Vg_exp, ax_Id_exp, ax_mu_exp, ax_SS_exp, ax_Vd_exp = [], [], [], [], []
-fnames = ['ITO_IdVG_param_Vd [(1) _RoomT measurement for 2R sample with Vd param_; 12_18_2022 1_45_33 PM].csv']
-# fnames = ['ITO_IdVG [(26) _RoomT measurement for 2R sample_; 12_18_2022 1_33_51 PM].csv']
+fnames = ['ITO_IdVG [(26) _RoomT measurement for 2R sample_; 12_18_2022 1_33_51 PM].csv']
 # fnames = ['ITO_IdVG [(10) _T = -20C 2R sample_; 12_18_2022 3_17_46 PM].csv']
 # fnames = ['ITO_IdVG [(14) _T = -10C 2R sample_; 12_18_2022 3_29_42 PM].csv']
 # fnames = ['ITO_IdVG [(17) _T=0C 2R sample_; 12_18_2022 3_35_50 PM].csv']
@@ -102,29 +93,28 @@ for fname in fnames:
      L=1.5; W =20; 
      kDE=16; tDE=5.3e-9; Cox = epso*kDE/tDE;
 
-     ax_Vg_exp += [d_Vg[d_Vg >= Vg_min] for d_Vg in Vg_data[:]]
-     ax_Id_exp += [d_Id[d_Vg >= Vg_min] for d_Vg, d_Id in zip(Vg_data[:], Id_data[:])]
-     ax_Vd_exp += [d_Vd[d_Vg >= Vg_min] for d_Vg, d_Vd in zip(Vg_data[:], Vd_data[:])]
+     ax_Vg_exp += [d_Vg[d_Vg >= Vg_min] for d_Vg in Vg_data[0:1]]
+     ax_Id_exp += [d_Id[d_Vg >= Vg_min] for d_Vg, d_Id in zip(Vg_data[0:1], Id_data[0:1])]
+     ax_Vd_exp += [d_Vd[d_Vg >= Vg_min] for d_Vg, d_Vd in zip(Vg_data[0:1], Vd_data[0:1])]
      ax_mu_exp += \
           [1e4*(np.gradient(d_Id[d_Vg >= Vg_min])/np.gradient(d_Vg[d_Vg >= Vg_min]))/(Cox*(W/L)*d_Vd[d_Vg >= Vg_min])\
-          for d_Vg, d_Vd, d_Id in zip(Vg_data[:], Vd_data[:], Id_data[:])]
+          for d_Vg, d_Vd, d_Id in zip(Vg_data[0:1], Vd_data[0:1], Id_data[0:1])]
      ax_SS_exp += \
           [1e3*(np.gradient(d_Vg[d_Vg >= Vg_min])/np.gradient(log10(d_Id[d_Vg >= Vg_min])))\
-          for d_Vg, d_Id in zip(Vg_data[:], Id_data[:])]
+          for d_Vg, d_Id in zip(Vg_data[0:1], Id_data[0:1])]
 
 ax_Vg_sim, ax_Vd_sim, ax_Id_sim, ax_mu_sim, ax_SS_sim = [], [], [], [], []
-# ax_Vg_sim = ax_Vg_exp
-# ax_Vd_sim = ax_Vd_exp
-# ax_Id_sim = [ np.array([model(params=params,inputs=dict(Vgs=Vg,Vds=Vd))[1] for Vg, Vd in zip(Vg_arr, Vd_arr)])\
-#              for Vd_arr, Vg_arr in zip(ax_Vd_sim, ax_Vg_sim) ]
-# ax_mu_sim = [1e4*(np.gradient(Id_arr)/np.gradient(Vg_arr))/(Cox*(W/L)*Vd_arr)\
-#              for Vd_arr, Vg_arr, Id_arr in zip(ax_Vd_sim, ax_Vg_sim, ax_Id_sim)]
-# ax_SS_sim = [(np.gradient(Vg_arr)*1e3)/np.gradient(log10(Id_arr))\
-#              for Vg_arr, Id_arr in zip(ax_Vg_sim, ax_Id_sim)]
+ax_Vg_sim = ax_Vg_exp
+ax_Vd_sim = ax_Vd_exp
+ax_Id_sim = [ np.array([model(params=params,inputs=dict(Vgs=Vg,Vds=Vd))[1] for Vg, Vd in zip(Vg_arr, Vd_arr)])\
+             for Vd_arr, Vg_arr in zip(ax_Vd_sim, ax_Vg_sim) ]
+ax_mu_sim = [1e4*(np.gradient(Id_arr)/np.gradient(Vg_arr))/(Cox*(W/L)*Vd_arr)\
+             for Vd_arr, Vg_arr, Id_arr in zip(ax_Vd_sim, ax_Vg_sim, ax_Id_sim)]
+ax_SS_sim = [(np.gradient(Vg_arr)*1e3)/np.gradient(log10(Id_arr))\
+             for Vg_arr, Id_arr in zip(ax_Vg_sim, ax_Id_sim)]
 
-ax_Vg_hspice, ax_Vd_hspice, ax_Id_hspice, ax_mu_hspice, ax_SS_hspice = [], [], [], [], []
-ax_Vg_hspice = [np.array(Vg_acq_list) for Vg_acq_list in Vg_acq_lists]
-ax_Vd_hspice = [np.array(Vd_acq_list) for Vd_acq_list in Vd_acq_lists]
+ax_Vg_hspice = [np.array(Vg_acq_list) for Id_acq_list in Id_acq_lists]
+ax_Vd_hspice = [ax_Vd_exp[0] for Id_acq_list in Id_acq_lists]
 ax_Id_hspice = [np.array(Id_acq_list) for Id_acq_list in Id_acq_lists]
 ax_mu_hspice = [1e4*(np.gradient(Id_arr)/np.gradient(Vg_arr))/(Cox*(W/L)*Vd_arr)\
              for Vd_arr, Vg_arr, Id_arr in zip(ax_Vd_hspice, ax_Vg_hspice, ax_Id_hspice)]
@@ -135,15 +125,12 @@ ax_Vg = ax_Vg_exp + ax_Vg_sim + ax_Vg_hspice
 ax_Id = ax_Id_exp + ax_Id_sim + ax_Id_hspice
 ax_mu = ax_mu_exp + ax_mu_sim + ax_mu_hspice
 ax_SS = ax_SS_exp + ax_SS_sim + ax_SS_hspice
-
-c = [yy for xx in ['solid', 'dotted', 'dashed'] for yy in ['r', 'b', 'g', 'k', 'm']] 
-s = [xx for xx in ['solid', 'dotted', 'dashed'] for yy in ['r', 'b', 'g', 'k', 'm']]
-print(c, s)
-lin_plot(x=ax_Vg, y=ax_Id, c=c, s=s)
-logy_plot(x=ax_Vg, y=ax_Id, c=c, s=s)
-lin_plot(x=ax_Vg, y=ax_mu, c=c, s=s)
-# logy_plot(x=ax_Vg, y=ax_mu, c=c, s=s)
-logx_plot(x=ax_Id, y=ax_SS, c=c, s=s,\
+    
+lin_plot(x=ax_Vg, y=ax_Id, c=['r','b','r','b'], s=['solid','solid','dashdot','dashdot'])
+logy_plot(x=ax_Vg, y=ax_Id, c=['r','b','r','b'], s=['solid','solid','dashdot','dashdot'])
+lin_plot(x=ax_Vg, y=ax_mu, c=['r','b','r','b'], s=['solid','solid','dashdot','dashdot'])
+logy_plot(x=ax_Vg, y=ax_mu, c=['r','b','r','b'], s=['solid','solid','dashdot','dashdot'])
+logx_plot(x=ax_Id, y=ax_SS, c=['r','b','r','b'], s=['solid','solid','dashdot','dashdot'],\
     xlim=(W*1e-13, W*1e-7), ylim=(45, 200))
 
 # print(ax_Id_sim[0])
